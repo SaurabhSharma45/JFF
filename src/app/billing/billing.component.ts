5fa081e4781b5c03d24246ede613cbba5a9eebb6
Saurabh Sharma <saurabh.sharma@kdi.kongsberg.com>
2019-05-24 06:16:20 +0530

    new changes.

--- src/app/billing/billing.component.ts
+++ src/app/billing/billing.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-billing',
   templateUrl: './billing.component.html',
   styleUrls: ['./billing.component.css']
 })
-export class BillingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class BillingComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    console.log(this.model);
+    localStorage.setItem('model', JSON.stringify(this.model))
   }
+  constructor() { }
+  model = {};
+  ngOnInit() {
+    if(localStorage.getItem('model')){
+      this.model = JSON.parse(localStorage.getItem('model'));
+    }
+    }
 
 }
